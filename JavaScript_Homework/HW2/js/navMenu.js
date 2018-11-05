0bfe6eb42e8b389873c6c8b6e5cf02f8ef58d6d4
LesleyChang <lesleychang1024@gmail.com>
2018-11-05 21:38:50 +0800

    reduce nav menu
    segment <header> to navMenu.js

---
+++ JavaScript_Homework/HW2/js/navMenu.js
@@ -0,0 +1,16 @@
+﻿document.write(
+    '<header>' +
+        '<div class="header">' +
+            '<h1 class="title">JavaScriptHomework</h1><br>' +
+                '<nav>' +
+                    '<ul class="menu">' +
+                        '<li><a href="index.html">回首頁</a></li>' +
+                        '<li><a href="index_P1_multi-table.html">九九乘法表</a></li>' +
+                        '<li><a href="index_P2_form.html">表單欄位驗證</a></li>' +
+                        '<li><a href="index_P3_score.html">評價</a></li>' +
+                        '<li><a href="index_P4_ads.html">廣告輪播</a></li>' +
+                        '<li><a href="index_P5_calendar.html">選擇日曆</a></li>' +
+                     '</ul>' +
+                '</nav>' +
+        '</div>' +
+    '</header>');
